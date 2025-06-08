3f4ed53b696363348ee6e01c2a1dcd55be5035c9
santiagosoft02 <santiagosoft02@gmail.com>
2025-06-08 04:39:09 +0000

    website CV v1.0.0

---
+++ js/lenguajes.js
@@ -0,0 +1,124 @@
+function infoJava() {
+    const app = document.getElementById("main");
+  
+    app.innerHTML = 
+    `
+      <div class="contenido-conocimiento">
+        <h1>Java</h1>
+        <div class="desc-conocimiento">
+          <img src="imgs/conocimientos/java.png" alt="" class="img-desc-cono">
+          <p>  Sólidos conocimientos en desarrollo backend.
+               Experiencia en estructuras de programación, manejo de variables 
+               y diversos tipos de código, incluyendo 
+               programación orientada a objetos (POO). 
+               Competente en la conexión y automatización de procesos con 
+               bases de datos utilizando PHP.
+          </p>
+        </div>
+      </div>
+    `;
+  }
+
+  function infoGcloud() {
+    const app = document.getElementById("main");
+  
+    app.innerHTML = 
+    `
+      <div class="contenido-conocimiento">
+        <h1>Google Cloud</h1>
+        <div class="desc-conocimiento">
+          <img src="imgs/conocimientos/gcloud.png" alt="" class="img-desc-cono">
+          <p>  Dentro del curso puesto en practica de google cloud
+             se tiene estudio de la teoria y practica basica 
+             (Creacion y modificacion no avanzada).
+          </p>
+          <ul>
+            <li>Maquinas virtuales y kubernetes</li>
+            <li>Infraestructura y almacenamiento de datos SQL y noSQL</li>
+            <li>Redes y seguridad de la nube (IAM, reglas de firewall y balanceadores de carga)</li>
+            <li>Machine learning e inteligencia artificial de google.</li>
+            <li>Control</li>
+            <li>VPCs</li>
+            <li>Supervision y control de rendimiento  (Cloud monitoring)</li>
+          </ul>
+        </div>
+      </div>
+    `;
+  }
+
+  function infoLinux() {
+    const app = document.getElementById("main");
+  
+    app.innerHTML = 
+    `
+      <div class="contenido-conocimiento">
+        <h1>Linux</h1>
+        <div class="desc-conocimiento">
+          <img src="imgs/conocimientos/linux.png" alt="" class="img-desc-cono">
+          <p>
+            Conocimiento del manejo del SO y de la terminal de linux.
+          </p>
+          <ul>
+            <li>Navegación de archivos dentro del sistema</li>
+            <li>Gestión de archivos y carpetas</li>
+            <li>Visualización y edición de archivos</li>
+            <li>Permisos y propiedad</li>
+            <li>Instalación de paquetes</li>
+            <li>Gestión de procesos</li>
+            <li>Usuarios y grupos</li>
+            <li>Compresión y descompresión</li>
+          </ul>
+        </div>
+      </div>
+    `;
+  }
+
+  function infoDB() {
+    const app = document.getElementById("main");
+  
+    app.innerHTML = 
+    `
+      <div class="contenido-conocimiento">
+        <h1>Base de datos relacionales</h1>
+        <div class="desc-conocimiento">
+          <img src="imgs/conocimientos/bd.png" alt="" class="img-desc-cono">
+          <p>Manejo de bases de datos relacionales a nivel de analista
+            y soporte administrativo. <br>
+            Experiencia en modelado de datos y diseño de esquemas 
+            con relaciones entre tablas. <br>
+            Dominio en la escritura de consultas SQL DDL y DML <br>
+            (UPDATE, DELETE, INSERT, SELECT, JOIN, GROUP BY, subconsultas, funciones agregadas,
+            CREATE, ALTER, DROP) <br>
+            Uso de herramientas com MySQL Workbench y Oracle SQL Developer para
+            el desarrollo y analisis de datos
+          </p>
+          <ul>
+            <li>Creacion de usuarios y permisos en diferentes esquemas</li>
+            <li>COMMIT, ROLLBACK y SAVEPOINT</li>
+            <li>Copias de seguridad y recuperacion basica</li>
+            <li>Optimizacion de consultas y uso de indices</li>
+            <li>TRIGGERS</li>
+          </ul>
+        </div>
+      </div>
+    `;
+  }
+
+  function infoFrontend() {
+    const app = document.getElementById("main");
+  
+    app.innerHTML = 
+    `
+      <div class="contenido-conocimiento">
+        <h1>Frontend</h1>
+        <div class="desc-conocimiento">
+          <img src="imgs/conocimientos/frontend.png" alt="" class="img-desc-cono">
+          <p> Dominio de HTML, CSS
+              y conocimientos funcionales en JavaScript. 
+              Experiencia en creación de interfaces limpias, 
+              responsivas y centradas en la experiencia del usuario.
+          </p>
+        </div>
+      </div>
+    `;
+  }
---
+++ js/main.js
@@ -0,0 +1,84 @@
+function sobremi() {
+  const seccion = document.querySelector('.section-sobremi');
+  if (seccion) {
+    seccion.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+
+function certificados() {
+  mostrarConAnimacion(`
+    <div class="contenido-general ">
+      <div class="certificados">
+        <a href="https://www.credly.com/badges/e07ff2dd-aff4-4faf-9318-00027745d4d9/linked_in_profile" class="certified" target="_blank">Cloud Computing Foundations</a>
+        <a href="https://skillshop.exceedlms.com/student/award/JT5BttgL1cjNSYKMNC8WwGKj" class="certified" target="_blank">HTML y CSS 1</a>
+        <a href="https://skillshop.exceedlms.com/student/award/7zLMm57RGiTZxz7EZzQWarDw" class="certified" target="_blank">HTML y CSS 2</a>
+        <p>Insignas en <a href="https://www.linkedin.com/in/santiago-castellanos-84913a249/" target="_blank">LinkedIN</a></p>
+      </div>
+      
+      <button class="boton-ocultar" onclick="ocultarGeneral()">Ocultar</button>
+    </div>
+  `);
+}
+
+
+function conocimientosinfo() {
+  mostrarConAnimacion(`
+    <div class="contenido-general">
+      <div class="botones-logos">     
+         
+        <button class="boton-conocimiento" onclick="infoDB()">
+          <img src="../imgs/conocimientos/bd.png" alt="" class="imagen-lenguaje">
+        </button>
+
+        <button class="boton-conocimiento" onclick="infoFrontend()">
+          <img src="../imgs/conocimientos/frontend.png" alt="" class="imagen-lenguaje">
+        </button>
+
+        <button class="boton-conocimiento" onclick="infoLinux()">
+          <img src="../imgs/conocimientos/linux.png" alt="" class="imagen-lenguaje">
+        </button>
+
+        <button class="boton-conocimiento" onclick="infoJava()">
+          <img src="../imgs/conocimientos/java.png" alt="" class="imagen-lenguaje">
+        </button>
+
+        <button class="boton-conocimiento" onclick="infoGcloud()">
+          <img src="../imgs/conocimientos/gcloud.png" alt="" class="imagen-lenguaje">
+        </button>
+      </div>
+      <button class="boton-ocultar" onclick="ocultarGeneral()">Ocultar</button>
+    </div>
+  `);
+}
+
+  
+
+  // Funcion boton ocultar seccion de Conocimientos
+  function ocultarGeneral() {
+    const app = document.getElementById("info-nav");
+    app.innerHTML = ""; // Limpia el contenido
+  }
+  
+  function activarBoton(boton) {
+    // Remueve la clase 'active' de todos los botones
+    const botones = document.querySelectorAll('.c-nav-item');
+    botones.forEach(b => b.classList.remove('active'));
+  
+    // Agrega la clase 'active' al botón clicado
+    boton.classList.add('active');
+  }
+  
+  // Animacion al presionar los botones nav
+  function mostrarConAnimacion(html) {
+    const app = document.getElementById("info-nav");
+    app.innerHTML = html;
+  
+    setTimeout(() => {
+      const contenido = document.querySelector('.contenido-general');
+      if (contenido) {
+        contenido.classList.add("mostrar");
+      }
+    }, 10);
+  }
+  
